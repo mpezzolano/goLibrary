ab32432ece65d5544e90fbf47ae46a8f0e2c3105
Marcelo Pezzolano <mpezzolano@gmail.com>
2024-05-28 07:25:40 -0300

    Add files utils

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"goLibrary/fileutils"
+	_ "goLibrary/fileutils"
 	"goLibrary/logger"
 	_ "goLibrary/logger"
 )
@@ -11,5 +13,13 @@ func main() {
 	log.Warning("This is a warning message")
 	log.Error("This is an error message")
 	log.Debug("This is a debug message")
+
+	// Writing to a file
+	err := fileutils.WriteFile("example.txt", "This is an example content")
+	if err != nil {
+		log.Error("Failed to write file: " + err.Error())
+	} else {
+		log.Info("File written successfully")
+	}
 
 }
